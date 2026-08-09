c6c864317b1b77cd1c7153be3487b2e490180c4b
ricardostarosa84 <ricardostarosa84@gmail.com>
2026-08-09 18:05:58 -0300

    classe teste custom

--- src/test/java/com/algaworks/algashop/ordering/CustomerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/CustomerTest.java
@@ -2,28 +2,48 @@ package com.algaworks.algashop.ordering;
 
 import com.algaworks.algashop.ordering.domain.entity.Customer;
 import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.util.UUID;
 
 public class CustomerTest {
 
     @Test
-    public void testingCustomer() {
+    void given_invalidEmail_whenTryCreateCustomer_shouldGenerateException() {
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(()-> {
+                    new Customer(
+                            IdGenerator.generateTimeBasedUUID(),
+                            "John Doe",
+                            LocalDate.of(1991, 7, 5),
+                            "invalid",
+                            "478-256-2504",
+                            "255-08-0578",
+                            false,
+                            OffsetDateTime.now()
+                    );
+                });
+    }
+
+    @Test
+    void given_invalidEmail_whenTryUpdatedCustomerEmail_shouldGenerateException() {
         Customer customer = new Customer(
                 IdGenerator.generateTimeBasedUUID(),
-                "Jhon Doe",
-                LocalDate.of(1991, 7,5),
-                "jhon.doe@email.com",
+                "John Doe",
+                LocalDate.of(1991, 7, 5),
+                "john.doe@gmail.com",
                 "478-256-2504",
                 "255-08-0578",
-                true,
+                false,
                 OffsetDateTime.now()
         );
 
-        customer.addLoyaltyPoints(10);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(()-> {
+                    customer.changeEmail("invalid");
+                });
     }
 
 }
